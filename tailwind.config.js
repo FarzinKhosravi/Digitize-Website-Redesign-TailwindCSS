1763ad6086e35d07709f81f5e397edd935bf3cb0
FarzinKhosravi <khosravii.farzin@gmail.com>
2023-10-09 15:15:46 +0330

    changed single-product section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,24 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      maxWidth: {
+        65: "260px",
+      },
+
       inset: {
         38: "152px",
       },
+
       fontSize: {
         xxs: "0.7rem",
       },
+
       padding: {
         1.8: "0.45rem",
         4.5: "18px",
         3.4: "0.85rem",
       },
+
       height: {
         30: "7.5rem",
       },
